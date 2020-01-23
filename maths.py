2fe8d53251bcf97d03898a6f33667c183ccde085
mn1del <mn1del@users.noreply.github.com>
2020-01-23 11:33:40 +0000

    add make_all_sentences
    make_all_sentences shuffles the basic sentence and returns all variants. E.g. (x+y=z, y+x=z, z-y=x, z-x=y)

--- maths.py
+++ maths.py
@@ -54,7 +54,7 @@ def get_tens_partition(z_range=(11,100)):
     y = z - x
     return ("+", x, y, z)
 
-def make_simple_sentance(operator, x, y, z, solve_for=("x", "y", "z"), line_break=True):
+def make_simple_sentence(operator, x, y, z, solve_for=("x", "y", "z"), line_break=True):
     """
     return string in the format "x + y = z"
 
@@ -65,19 +65,59 @@ def make_simple_sentance(operator, x, y, z, solve_for=("x", "y", "z"), line_brea
         z: (number)
         solve_for: (list like) values must be "x"|"y"|"z". Determines which number to solve for.
                    If multiple values are supplied, one will be randomly chosen
-        line_break: (bool) If True, add a line break at the end of the sentance           
+        line_break: (bool) If True, add a line break at the end of the sentence           
     """
+    assert eval("{}{}{}=={}".format(x,operator,y,z))
     if type(solve_for) == str:
         solve_for = list(solve_for)
     solve_for = solve_for[randint(0, len(solve_for)-1)]    
     if solve_for == "z":
-        sentance = "{} {} {} = ___".format(x, operator, y)
+        sentence = "{} {} {} = ___".format(x, operator, y)
     elif solve_for == "x":
-        sentance = "___ {} {} = {}".format(operator, y, z)
+        sentence = "___ {} {} = {}".format(operator, y, z)
     elif solve_for == "y":
-        sentance = "{} {} ___ = {}".format(x, operator, z)
+        sentence = "{} {} ___ = {}".format(x, operator, z)
     if line_break:
-        sentance = "{}\n".format(sentance)
-    return sentance
+        sentence = "{}\n".format(sentence)
+    return sentence
 
+def make_all_sentences(operator, x, y, z, solve_for=("x", "y", "z"), line_break=True):
+    """
+    Return a list of all possible sentences formed using x, y, z
 
+    args:
+        operator: (str) e.g. "+"
+        x: (number)
+        y: (number)
+        z: (number)
+        solve_for: (list like) values must be "x"|"y"|"z". Determines which number to solve for.
+                   If multiple values are supplied, one will be randomly chosen
+        line_break: (bool) If True, add a line break at the end of the sentence           
+    """
+    assert eval("{}{}{}=={}".format(x,operator,y,z))
+    def flip_operator(op):
+        if op == "-":
+            return "+"
+        elif op == "+":
+            return "-"
+        elif op == "/":
+            return "*"
+        elif op == "*":
+            return "/"
+        else:
+            print("Check Operator!")
+
+    if type(solve_for) == str:
+        solve_for = list(solve_for)
+    variants = []
+    # start with positive operators
+    if operator in ["-", "/"]:
+        operator = flip_operator(operator)
+        y = z
+        z = y
+    variants.append(make_simple_sentence(operator, x, y, z, solve_for=("x", "y", "z"), line_break=True)   
+    variants.append(make_simple_sentence(operator, y, x, z, solve_for=("x", "y", "z"), line_break=True)   
+    variants.append(make_simple_sentence(flip_operator(operator), z, y, x, solve_for=("x", "y", "z"), line_break=True)   
+    variants.append(make_simple_sentence(flip_operator(operator), z, x, y, solve_for=("x", "y", "z"), line_break=True)   
+    return variants    
+        
